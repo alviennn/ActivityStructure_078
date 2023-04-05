d8a9a588ef6206a4880bbf42aa87c9d40bd3b7a8
alviennn <alvien.rnp@gmail.com>
2023-04-05 10:42:06 +0700

    Menambahkan prosedur main dengan tipe integer

--- Latihan1/Latihan1.cpp
+++ Latihan1/Latihan1.cpp
@@ -8,3 +8,31 @@ struct Mahasiswa
 	string alamat;
 	int umur;
 };
+
+int main() {
+	Mahasiswa mhs, mhs2;
+
+	mhs.nim = "20220140078";
+	mhs.umur = 18;
+	mhs.nama = "Alvien Ridho";
+	mhs.alamat = "Banjarmasin";
+
+	cout << "Masukkan NIM : ";
+	cin >> mhs2.nim;
+	cout << "Masukkan Umur : ";
+	cin >> mhs2.umur;
+	cout << "Masukkan Nama : ";
+	cin >> mhs2.nama;
+	cout << "Masukkan Alamat : ";
+	cin >> mhs2.alamat;
+
+	cout << "NIM : " << mhs.nim;
+	cout << "\nUmur : " << mhs.umur;
+	cout << "\nNama : " << mhs.nama;
+	cout << "\nAlamat : " << mhs.alamat;
+
+	cout << "NIM : " << mhs2.nim;
+	cout << "\nUmur : " << mhs2.umur;
+	cout << "\nNama : " << mhs2.nama;
+	cout << "\nAlamat : " << mhs2.alamat;
+}
